ccd92962a49c7d2935e498c2e3c0acc74a9d0ee0
bghbmh <bghbmh@daum.net>
2025-07-03 14:57:17 +0900

    test11

--- public/assets/js/web-components.js
+++ public/assets/js/web-components.js
@@ -1,59 +1,41 @@
 
 //단일 컴포넌트
-import { origin, testDB, memberDB  } from '../../../tempFile.js';
-import '../../components/projectDetail.js';
-import '../../components/page-header.js';
-import '../../components/header-user-info.js';
-import '../../components/global-nav.js';
 
-import '../../components/member-profile.js'
+import '/components/projectDetail.js';
+import '/components/page-header.js';
+import '/components/header-user-info.js';
+import '/components/global-nav.js';
 
-import '../../components/cardType2.js';
-//import../.. '/components/cardType3.js';
-
-import '../../components/list-item-type1.js';
-
-import '../../components/list-item-type3.js';
-
-import '../../components/barChartType1.js';
-
-import '../../components/boardList.js';
-
-import '../../components/profile-info.js';
-
-// 복합 ../..컴포넌트
-import '../../components/mainSection1.js';
-import '../../components/mainSection2.js';
-import '../../components/mainSection3.js';
-
-import '../../components/complete-items.js';
-
-import '../../components/project-list.js';
-import '../../components/layout-components.js';
-import '../../components/loading-skeleton.js';
-
-import '../../components/modal-popup.js';
-import '../../components/pop-over.js';
+import '/components/member-profile.js'
 
 
 
 
+import '/components/cardType2.js';
+//import '/components/cardType3.js';
 
 
+import '/components/list-item-type1.js';
 
-import '../../components/personal-info.js';
-import '../../components/site-account.js';
+import '/components/list-item-type3.js';
 
-import '../../components/current-state.js';
-import '../../components/project-category.js';
-import '../../components/extra-info.js';
-import '../../components/external-link.js';
-import '../../components/project-member.js'
-import '../../components/preview-file.js'
-import '../../components/file-uploader.js';
-import '../../components/switch-type1.js';
-import '../../components/category-type2.js';
-import '../../components/category-type3.js';
-import '../../components/radio-group-list1.js';
-import '../../components/text-icon.js';
-import '../../components/alert-toast.js';
+import '/components/barChartType1.js';
+
+import '/components/boardList.js';
+
+import '/components/profile-info.js';
+
+// 복합 컴포넌트
+import '/components/mainSection1.js';
+import '/components/mainSection2.js';
+import '/components/mainSection3.js';
+
+import '/components/complete-items.js';
+
+import '/components/project-list.js';
+import '/components/layout-components.js';
+
+import '/components/loading-skeleton.js';
+
+import '/components/modal-popup.js';
+import '/components/pop-over.js';
